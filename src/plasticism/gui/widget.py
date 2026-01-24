44b3ba64cf7b9f25d4264e9e482689dd3691924e
ChenWuwei404 <Uplantting@163.com>
2026-01-24 19:59:31 +0800

    feat(gui): Create `Widget` base class

---
+++ src/plasticism/gui/widget.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+class Widget:
+    def __init__(self) -> None:
+        self.parent: Optional[Widget] = None
+        self.children: list[Widget] = []
+
+    def set_parent(self, parent: Optional['Widget']) -> None:
+        self.parent = parent
+
+    def get_parent(self) -> 'Widget':
+        if self.parent is None:
+            raise ValueError(f"{repr(self)} has no parent.")
+        return self.parent
+    
+    def add_child(self, child: 'Widget') -> None:
+        child.set_parent(self)
+        self.children.append(child)
+
+    def remove_child(self, child: 'Widget') -> None:
+        child.set_parent(None)
+        self.children.remove(child)
+
+    def get_children(self) -> list['Widget']:
+        return self.children
