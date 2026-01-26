a76204fabc50acc6d880fd6a63bebc688290a859
ChenWuwei404 <uplantting@163.com>
2026-01-26 17:34:38 +0800

    feat(assigner): Set `Assigner` as a subclass of `Processor`

--- src/plasticism/core/assigner.py
+++ src/plasticism/core/assigner.py
@@ -1,6 +1,6 @@
 from typing import TypeVar, Generic, Type
 
-from plasticism.core.event import Event
+from plasticism.core.event import Event, EventBundle, Processor
 from plasticism.core.trigger import Trigger
 
 T = TypeVar("T", bound=Event)
@@ -26,7 +26,7 @@ class AssignerItem(Generic[T]):
     def clear(self) -> None:
         self.triggers.clear()
 
-class Assigner:
+class Assigner(Processor):
     def __init__(self) -> None:
         self.items: list[AssignerItem] = []
 
@@ -35,6 +35,10 @@ class Assigner:
 
     def remove_item(self, item: AssignerItem) -> None:
         self.items.remove(item)
+
+    def process_event(self, event_bundle: EventBundle) -> None:
+        for event in event_bundle:
+            self.process(event)
 
     def process(self, event: Event) -> None:
         for item in self.items:
