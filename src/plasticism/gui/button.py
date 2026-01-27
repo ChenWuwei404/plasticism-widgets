94c562ae5b8a92c4cd4fdbe9bb27a2a09406a848
ChenWuwei404 <uplantting@163.com>
2026-01-27 13:42:49 +0800

    feat(button): Add `button` for `ButtonClick` event

--- src/plasticism/gui/button.py
+++ src/plasticism/gui/button.py
@@ -2,9 +2,9 @@ from plasticism.core.transition import State, Transition, Machine
 from plasticism.core.transition import class_judger
 
 from plasticism.gui.widget import MouseEnter, MouseLeave
-from plasticism.core.event import MousePress, MouseRelease, LocalEvent, MouseEvent
+from plasticism.core.event import MousePress, MouseRelease, LocalEvent, MouseEvent, MouseButton
 
-class ButtonClick(LocalEvent, MouseEvent):
+class ButtonClick(LocalEvent, MouseButton):
     pass
 
 class ButtonMachine(Machine):
@@ -32,7 +32,7 @@ class ButtonMachine(Machine):
         self.pressed.add_transition(Transition(
             self.hover,
             class_judger(MouseRelease),
-            lambda e: ButtonClick(e.x, e.y),  # type: ignore
+            lambda e: ButtonClick(e.x, e.y, e.button),  # type: ignore
         ))
 
         self.pressed.add_transition(Transition(
