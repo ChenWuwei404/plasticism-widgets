267a5cf715896053a7a8ab69c02774fa4777b56b
ChenWuwei404 <uplantting@163.com>
2026-02-05 09:55:27 +0800

    feat(window): Add root widget feats

--- src/plasticism/gui/window.py
+++ src/plasticism/gui/window.py
@@ -14,11 +14,12 @@ if sys.platform == "win32":
     from plasticism.core.system import set_titlebar_color, set_border_color
 
 class Window:
-    def __init__(self, size: tuple[int, int], flags: int = 0, vsync: int = 0) -> None:
+    def __init__(self, size: tuple[int, int], widget: Widget, flags: int = 0, vsync: int = 0) -> None:
         pygame.init() if not pygame.get_init() else None
+        self.set_root_widget(widget)
 
-        self.size = size
-        display.set_mode(size, flags=flags, vsync=vsync)
+        self.set_size(size)
+        display.set_mode(self.size, flags=flags, vsync=vsync)
         self.hwnd = display.get_wm_info()["window"]
         self.set_title("Plasticism Window")
         self.set_icon(Surface((0, 0), SRCALPHA))
@@ -49,17 +50,24 @@ class Window:
         self.border_color = color
         set_border_color(self.hwnd, color)
 
+    def set_size(self, size: tuple[int, int], resized: bool = True) -> None:
+        self.size = size
+        self.root_widget.set_max_size(size)
+        display.set_mode(size) if not resized else None
 
     def set_running(self, running: bool) -> None:
         self.running = running
     
     def set_root_widget(self, widget: Widget) -> None:
         self.root_widget = widget
+        self.root_widget.set_size((-1, -1))
 
     def exec(self) -> None:
         while self.running:
-            for py_event in pygame.event.get():
-                event = generate_event(py_event)
+            for pg_event in pygame.event.get():
+                if pg_event.type == pygame.VIDEORESIZE:
+                    self.set_size((pg_event.w, pg_event.h), resized=True)
+                event = generate_event(pg_event)
                 self.root_widget.tunnel_event(event)
 
     
