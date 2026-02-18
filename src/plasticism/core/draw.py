fe3f3f495dee26e91345858d970440becc545f7a
ChenWuwei404 <Uplantting@163.com>
2026-02-18 20:07:13 +0800

    feat(draw): Rename and implement filled and outline rectangle drawing functions

--- src/plasticism/core/draw.py
+++ src/plasticism/core/draw.py
@@ -1,7 +1,8 @@
 from plasticism.core.surface_clip import SurfaceClip
 from pygame import Color, Rect
+from pygame import draw as pygame_draw
 
-def rect(surface_clip: SurfaceClip, color: Color, rect: Rect):
+def rect_filled(surface_clip: SurfaceClip, color: Color, rect: Rect):
     surface = surface_clip.surface
     pygame_rect = Rect(
         rect.x + surface_clip.clip_rect.x,
@@ -11,3 +12,12 @@ def rect(surface_clip: SurfaceClip, color: Color, rect: Rect):
     )
     surface.fill(color, pygame_rect)
 
+def rect_outline(surface_clip: SurfaceClip, color: Color, rect: Rect, width: int):
+    surface = surface_clip.surface
+    pygame_rect = Rect(
+        rect.x + surface_clip.clip_rect.x,
+        rect.y + surface_clip.clip_rect.y,
+        rect.width,
+        rect.height,
+    )
+    pygame_draw.rect(surface, color, pygame_rect, width)
