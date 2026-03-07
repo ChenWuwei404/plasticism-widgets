e8fd2b948183b135d8a0bb9b0ff78f3e36bb906f
ChenWuwei404 <uplantting@163.com>
2026-03-07 10:59:28 +0800

    refact(docs): Make `labels.py` more pythonic

--- examples/labels.py
+++ examples/labels.py
@@ -4,27 +4,31 @@ from plasticism.widget import Widget, Label, FixedScaleLabel
 
 from plasticism.core.layout import AlignHorizontal
 
-class Background(BackgroundBase):
+from pygame import RESIZABLE
+
+class Root(BackgroundBase):
     def get_background_color(self):
         return (0, 0, 0)
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.label1 = Label("This is a Label")
+        self.label1.set_x(10)
+        self.label1.set_y(10)
+        self.label1.set_width(-1)
+        self.label1.set_text_align_horizontal(AlignHorizontal.CENTER)
 
-root = Background()
+        self.label2 = FixedScaleLabel("Another Label, but will not scale with DPI")
+        self.label2.set_x(10)
+        self.label2.set_y(50)
+        self.label2.set_width(-1)
+        self.label2.set_text_align_horizontal(AlignHorizontal.CENTER)
 
-label1 = Label("This is a Label")
-label1.set_x(10)
-label1.set_y(10)
-label1.set_width(-1)
-label1.set_text_align_horizontal(AlignHorizontal.CENTER)
+        self.add_child(self.label1)
+        self.add_child(self.label2)
 
-label2 = FixedScaleLabel("Another Label, but will not scale with DPI")
-label2.set_x(10)
-label2.set_y(50)
-label2.set_width(-1)
-label2.set_text_align_horizontal(AlignHorizontal.CENTER)
+root = Root()
 
-root.add_child(label1)
-root.add_child(label2)
-
-window = Window((1280, 720), root)
+window = Window((1280, 720), root, RESIZABLE)
 
 window.exec()
